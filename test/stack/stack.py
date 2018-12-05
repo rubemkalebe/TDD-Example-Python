6bcd18d7dd7c5ff97bc62fe28deaf6cbfe8a324c
Rubem Kalebe Santos <rubemkalebe@gmail.com>
2018-12-05 11:54:18 -0300

    Refatorando testes #iteracao3

--- test/stack/stack.py
+++ test/stack/stack.py
@@ -4,28 +4,28 @@ from src.stack import Stack
 
 
 class StackTest(unittest.TestCase):
+    def setUp(self):
+        self.s = Stack()
+
     def test_empty_stack(self):
-        s = Stack()
-        self.assertTrue(s.is_empty())
-        self.assertEqual(0, s.size())
+        self.assertTrue(self.s.is_empty())
+        self.assertEqual(0, self.s.size())
 
     def test_push(self):
-        s = Stack()
-        s.push("first")
-        self.assertFalse(s.is_empty())
-        self.assertEqual(1, s.size())
-        self.assertTrue("first", s.top())
+        self.s.push("first")
+        self.assertFalse(self.s.is_empty())
+        self.assertEqual(1, self.s.size())
+        self.assertTrue("first", self.s.top())
 
     def test_push_and_pop(self):
-        s = Stack()
-        s.push("first")
-        s.push("second")
-        self.assertEqual(2, s.size())
-        self.assertTrue("second", s.top())
+        self.s.push("first")
+        self.s.push("second")
+        self.assertEqual(2, self.s.size())
+        self.assertTrue("second", self.s.top())
 
-        removed = s.pop()
-        self.assertEqual(1, s.size())
-        self.assertEqual("first", s.top())
+        removed = self.s.pop()
+        self.assertEqual(1, self.s.size())
+        self.assertEqual("first", self.s.top())
         self.assertEqual("second", removed)
 
 
